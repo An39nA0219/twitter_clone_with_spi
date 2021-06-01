fd77c2d56a3351fb8da4273bec380a2dcfa0bf71
An39nA0219 <an39na10282004@yahoo.co.jp>
2021-06-02 01:28:59 +0900

    add namespace "users"

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  post 'login', to: "user_jwts#create"
+  namespace "users" do
+    post 'login', to: "jwts#create"
+  end
 end
